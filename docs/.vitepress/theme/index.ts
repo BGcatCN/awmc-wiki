4981eb83f92a96dc9701e23a7393e33ea067d95d
Michaelwucoc <110759450+Michaelwucoc@users.noreply.github.com>
2026-03-30 00:06:01 +0800

    Add ReadingTime component to display word count and estimated reading time

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,17 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import StatChart from './components/StatChart.vue'
+import ReadingTime from './components/ReadingTime.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('StatChart', StatChart)
+    app.component('ReadingTime', ReadingTime)
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': () => h(ReadingTime)
+    })
   }
 }
